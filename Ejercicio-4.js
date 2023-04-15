6d3ffdba93b2b62873837a00a95771799019bf1b
Kevin07moya <danielmoya07@outlook.com>
2023-04-15 13:13:11 -0500

    Ejercicio-4

---
+++ Ejercicio-4.js
@@ -0,0 +1,27 @@
+let cadenaTexto = "Hola mundo!";
+console.log(cadenaTexto);
+console.log(typeof(cadenaTexto));
+
+//let nombre = "camilo";
+//cadenaTexto = "Hola!"+ "mi nombre es " + nombre;
+console.log(cadenaTexto);
+
+//let edad = 23;
+//console.log(`${nombre} tiene ${edad} años`);
+//console.log(typeof(edad));
+
+let verdadero = false;
+console.log("El verdadero se define con ",verdadero);
+console.log(typeof(verdadero));
+
+let color 
+console.log(`El color favoritpo es ${color}`);
+console.log(typeof(color));
+
+let nombre ="Diego";
+let apellido = "Hernandez";
+let edad = 22;
+let cancion = "la cucaracha";
+let respuesta = true;
+
+console.log(`Hola! Mi nombre es ${nombre} ${apellido} y tengo ${edad}, mi cancion favorita es ${cancion} y la respuesta es ${respuesta}`);
