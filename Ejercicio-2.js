7711473750242fc9b7857ca81a3eab6a3efafa1f
Kevin07moya <danielmoya07@outlook.com>
2023-04-14 19:07:42 -0500

    Se agrego los ; para tener manejo de las buenas practicas

--- Ejercicio-2.js
+++ Ejercicio-2.js
@@ -1,40 +1,40 @@
 var pelicula="interestelar"
 console.log("variable pelicula antes del bloque:", pelicula);
 
-let musica = "Rock"
+let musica = "Rock";
 console.log("variable musica antes del bloque:", musica);
 
 {
-    var pelicula = "Matilda"
-    console.log("variable pelicula antes del bloque:", pelicula)
+    var pelicula = "Matilda";
+    console.log("variable pelicula antes del bloque:", pelicula);
 
-    let musica = "Salsa"
-    console.log("variable musica antes del bloque:", musica)
+    let musica = "Salsa";
+    console.log("variable musica antes del bloque:", musica);
 
 }
 
 console.log("variable pelicula antes del bloque:", pelicula);
-console.log("variable musica antes del  bloque:", musica)
+console.log("variable musica antes del  bloque:", musica);
 
-var nombre = "Pepito"
-var apellido = "Perez"
+var nombre = "Pepito";
+var apellido = "Perez";
 
-let edad = "38"
-let comida = "pizza"
+let edad = "38";
+let comida = "pizza";
 
 {
-    var nombre = "Fulano"
-    let apellido = "de tal"
-    let edad = "40"
-    let comida = "hamburguesa"
+    var nombre = "Fulano";
+    let apellido = "de tal";
+    let edad = "40";
+    let comida = "hamburguesa";
 
-    console.log("la variable global se puede leer aqui", nombre)
-    console.log("la variable global se puede leer aqui", apellido)
-    console.log("la variable global se puede leer aqui", edad)
-    console.log("la variable global se puede leer aqui", comida)
+    console.log("la variable global se puede leer aqui", nombre);
+    console.log("la variable global se puede leer aqui", apellido);
+    console.log("la variable global se puede leer aqui", edad);
+    console.log("la variable global se puede leer aqui", comida);
 
 }
-console.log("la variable global se puede leer aqui", nombre)
-console.log("la variable global se puede leer aqui", apellido)
-console.log("la variable global se puede leer aqui", edad)
-console.log("la variable global se puede leer aqui", comida)
+console.log("la variable global se puede leer aqui", nombre);
+console.log("la variable global se puede leer aqui", apellido);
+console.log("la variable global se puede leer aqui", edad);
+console.log("la variable global se puede leer aqui", comida);
--- Ejercicio-3.js
+++ Ejercicio-3.js
@@ -1,11 +1,12 @@
 const PI = 3.1416;
 
-console.log(`El valor de PI es:  ${PI}`)
+console.log(`El valor de PI es:  ${PI}`);
 
-const seg_en_minutos = 60
-const minutos_en_hora = 60
-const numero_de_hora_en_dia =24
 
-console.log(`un minuto tiene ${seg_en_minutos} segundos, una hora tiene ${minutos_en_hora} minutos y un dia tiene ${numero_de_hora_en_dia} horas`)
+const seg_en_minutos = 60;
+const minutos_en_hora = 60;
+const numero_de_hora_en_dia =24;
+
+console.log(`un minuto tiene ${seg_en_minutos} segundos, una hora tiene ${minutos_en_hora} minutos y un dia tiene ${numero_de_hora_en_dia} horas`);
 
  
