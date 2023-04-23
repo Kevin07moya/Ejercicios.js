f2fb075ea358820946235f129d24e602ed2d83a8
Kevin07moya <danielmoya07@outlook.com>
2023-04-23 11:54:49 -0500

    agregar ejercicios de control

---
+++ funciones-guia.js
@@ -0,0 +1,28 @@
+let n1 = 14;
+let n2 = 40;
+
+function sumar(a, b) {
+    return a + b;
+}
+
+let num = sumar(n1, n2);
+console.log(num);
+
+
+function saludar (){
+    msj = "hola mundo"
+    return msj ;
+
+}
+
+console.log(saludar());
+
+
+function saludo (){
+    mssj = "Holiiss munduss"
+    return mssj ;
+
+}
+
+console.log(saludo());
+
